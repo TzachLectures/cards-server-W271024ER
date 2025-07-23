9486ce2de4c8069cec8148893bdcada372e5b686
NEW_USERNAME <bennydabush@gmail.com>
2025-07-23 18:25:42 +0300

    add cors pilicies

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import logger from "./middlewares/logger.js";
 import router from "./router/router.js";
-
+import cors from "cors";
 const app = express(); //איתחול אפליקציית אקספרס חדשה
 const port = 3000;
+
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500"],
+  })
+);
 
 app.use(express.json());
 app.use(logger);
