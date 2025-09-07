ba8adc73ea94615924baaf3bad6e1e98de738110
NEW_USERNAME <bennydabush@gmail.com>
2025-09-07 21:20:26 +0300

    config & atlas

--- DB/dbService.js
+++ DB/dbService.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose";
+import config from "config";
 
 export const connectToDb = () => {
+  const dbEnv = config.get("DB_ENVIRONMENT");
+
+  const connectionString =
+    dbEnv === "local" ? process.env.LOCAL_DB : process.env.ATLAS_DB;
+
+  const connectionMsg =
+    dbEnv === "local"
+      ? "connected to MongoDb Locally!"
+      : "connected to MongoDb Atlas!";
+
   mongoose
-    .connect("mongodb://127.0.0.1:27017/business_card_app")
-    .then(() => console.log("connected to MongoDb Locally!"))
+    .connect(connectionString)
+    .then(() => console.log(connectionMsg))
     .catch((error) => console.log(`could not connect to mongoDb: ${error}`));
 };
