3553d97711c10367c9513e59e92952b93f0f8fee
NEW_USERNAME <bennydabush@gmail.com>
2025-09-17 20:59:14 +0300

    add url to cors

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5173"],
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:5173",
+      "https://w271024er-cards.netlify.app",
+    ],
   })
 );
 
